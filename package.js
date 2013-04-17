b798b7d40931a7d91a2da3a7bb552e05e0451ecb
Abigail Watson <awatson1978@gmail.com>
2013-04-17 14:28:27 -0400

    trying to initialize from within the app

--- package.js
+++ package.js
@@ -3,8 +3,10 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+    api.use('templating','client');
+
     api.add_files('cordova.html', 'client');
     api.add_files('cordova.css', 'client');
     api.add_files('cordova.initialization.js', 'client');
-    api.add_files('cordova-2.6.0.js', 'client');
+    api.add_files('cordova-ios-2.6.0.js', 'client');
 });
