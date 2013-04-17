8169d754cb5e713a59fd59b5e024ea29802f55a1
Abigail Watson <awatson1978@gmail.com>
2013-04-17 14:44:08 -0400

    trying to initialize from package

--- cordova.initialization.js
+++ cordova.initialization.js
@@ -1,50 +1,36 @@
 //-------------------------------------------------------
 // Cordova App Object Literal
 
-if(Meteor.isClient){
-    app = {
-        // Application Constructor
-        initialize: function() {
-            this.bindEvents();
-        },
-        // Bind Event Listeners
-        //
-        // Bind any events that are required on startup. Common events are:
-        // 'load', 'deviceready', 'offline', and 'online'.
-        bindEvents: function() {
-            document.addEventListener('deviceready', this.onDeviceReady, false);
 
-            // This is an event that fires when a Cordova application is retrieved from the background.
-            document.addEventListener("resume", function() {
-                // http://docs.meteor.com/#meteor_reconnect
-                // Force an immediate reconnection attempt if the client is not connected to the server.
-                // This method does nothing if the client is already connected.
-                Meteor.reconnect();
-                //Meteor.resume();
-            });
-        },
-        // deviceready Event Handler
-        //
-        // The scope of 'this' is the event. In order to call the 'receivedEvent'
-        // function, we must explicity call 'app.receivedEvent(...);'
-        onDeviceReady: function() {
-            app.receivedEvent('deviceready');
-        },
-        // Update DOM on a Received Event
-        receivedEvent: function(id) {
-            var parentElement = document.getElementById(id);
-            var listeningElement = parentElement.querySelector('.listening');
-            var receivedElement = parentElement.querySelector('.received');
+app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-            console.log('Received Event: ' + id);
-        }
-    };
-
-    // this line must occur after the class literal object (above)
-    app.initialize(window);
-}
-
-
+        console.log('Received Event: ' + id);
+    }
+};
+app.initialize(window);
--- package.js
+++ package.js
@@ -8,5 +8,5 @@ Package.on_use(function (api) {
     api.add_files('cordova.html', 'client');
     api.add_files('cordova.css', 'client');
     api.add_files('cordova.initialization.js', 'client');
-    api.add_files('cordova-ios-2.6.0.js', 'client');
+    api.add_files('cordova-2.6.0.js', 'client');
 });
